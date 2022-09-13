8543f87aa1ae435092f32e2dd47494cade6c297a
doniaskima <doniaskima344@gmail.com>
2022-09-14 00:12:36 +0100

    gonna implement redux

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Chat.css";
+import MicNoneIcon from '@mui/icons-material/MicNone';
 
 const Chat = () => {
-  return (
-      <div className="chat">
-          
-   </div>
-  )
+    const [input, setInput] = useState('')
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        setInput("");
+    }
+    return (
+        <div className="chat">
+            <div className="chat-header">
+                <h4>To:<span className="chatName">Channel name</span></h4>
+                <strong>Details</strong>
+            </div>
+            <div className="chat-msg">
+                
+            </div>
+            
+            <div className="chatInput">
+                <form>
+                    <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+                    <button onClick={sendMessage}>Send Message</button>
+                </form>
+                <MicNoneIcon className="mic"/>
+            </div>
+
+        </div>
+    )
 }
 
 export default Chat
