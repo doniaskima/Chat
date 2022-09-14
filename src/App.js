27d3f233995646f7c1ac6a8275c79b1ee1141928
doniaskima <doniaskima344@gmail.com>
2022-09-14 14:59:17 +0100

    login functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React ,{useEffect} from 'react';
 import './App.css';
 import Imessage from './components/Imessage';
-import {selectUser} from "./features/userSlice";
-import { useSelector } from "react-redux";
+import {selectUser,login, logout} from "./features/userSlice";
+import {useDispatch, useSelector } from "react-redux";
 import Login from "./components/Login"
+import {auth} from "./components/firebase";
 
 function App() {
   const user = useSelector(selectUser);
+  const dispatch =useDispatch();
+  useEffect(()=>{
+       auth.onAuthStateChanged((authUser)=>{
+        if(authUser){
+         // user is logged in
+         dispatch(
+         login({
+            uid :authUser.uid,
+          photo:authUser.photoURL,
+          email: authUser.email,
+          displayName : authUser.displayName,
+            })
+         )
+        }else{
+        // user is logged out 
+        dispatch(logout())
+        }
+       })
+  },[])
   return (
     <div className="app">
     {user ?  <Imessage/> : <Login/>}
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -9,12 +9,12 @@ export const userSlice = createSlice({
         login: (state, action) => {
             state.user = action.payload;
         },
-        logout: state => {
+        logout: (state) => {
             state.user = null;
         }
     }
 })
 
 export const { login, logout } = userSlice.actions;
-export const selectUser = state => state.user.value;
+export const selectUser = (state) => state.user.user;
 export default userSlice.reducer;
