d6a60e8cd371487a380a0672d124f08d2124c5bd
doniaskima <doniaskima344@gmail.com>
2022-09-14 15:16:05 +0100

    login and logout are good

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,12 +4,15 @@ import { Avatar, IconButton } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import RateReviewIcon from '@mui/icons-material/RateReview';
 import SidebaChat from './SidebaChat';
-
+import {useSelector} from "react-redux"
+import {selectUser} from "../features/userSlice"
+import {auth} from "./firebase"
 const Sidebar = () => {
+    const user =useSelector(selectUser)
     return (
         <div className="sidebar">
             <div className="sidebar-header">
-                <Avatar className="sidebar-avatar"/>
+                <Avatar onClick={()=>auth.signOut()} src={user.photo} className="sidebar-avatar"/>
                 <div className="sidebar-input">
                     <SearchIcon />
                     <input placeholder="Search" />
